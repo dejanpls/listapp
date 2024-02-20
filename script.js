1126a9a13ee99ef460e4a3314cfb9b471c93f037
dejanpls <68787353+dejanpls@users.noreply.github.com>
2024-02-20 17:08:15 +0100

    Create function which adds text to a ul with a delete btn next to it, empty input or input with over 50 chars will be ignored

--- script.js
+++ script.js
@@ -2,3 +2,32 @@ const input = document.querySelector("input[type='text']");
 const button = document.querySelector("button");
 const list = document.querySelector(".list");
 
+const inputFunction = () => {
+	const inputItem = input.value;
+	input.value = "";
+
+	const listItem = document.createElement("li");
+	const listText = document.createElement("span");
+	const listBtn = document.createElement("button");
+
+	if (inputItem !== "" && inputItem.length <= 50) {
+		listText.textContent = inputItem;
+		listItem.appendChild(listText);
+
+		listItem.appendChild(listBtn);
+		listBtn.textContent = "Delete";
+
+		list.appendChild(listItem);
+	}
+
+	listBtn.addEventListener("click", () => {
+		list.removeChild(listItem);
+		input.focus();
+	});
+
+	input.focus();
+
+	console.log(inputItem.length);
+};
+
+button.addEventListener("click", inputFunction);
